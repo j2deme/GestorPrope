75997ae40d9061a0e131692c77d92bc327723e74
Jaime Jesús Delgado Meraz <j2deme@gmail.com>
2024-06-18 13:01:01 -0600

    Agrega link para aspirantes

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -15,6 +15,10 @@
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
+
+                    <x-nav-link :href="route('aspirantes.index')" :active="request()->routeIs('aspirantes.index')">
+                        Aspirantes
+                    </x-nav-link>
                 </div>
             </div>
 
