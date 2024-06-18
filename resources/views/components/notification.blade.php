754b61320958ecb429e6a4bf0183936c114cbc43
Jaime Jesús Delgado Meraz <j2deme@gmail.com>
2024-06-18 10:07:48 -0600

    Agrega componente para notificaciones

---
+++ resources/views/components/notification.blade.php
@@ -0,0 +1,32 @@
+@foreach (['success', 'warning', 'danger'] as $type)
+<x-alert :type="$type" class="leading-5 text-md">
+  @if ($type === 'warning')
+  <div class="flex items-center p-4 text-yellow-600 bg-yellow-100 rounded-md">
+    <div class="heroicon-icon">
+      <x-heroicon-o-exclamation-circle class="w-7 h-7" />
+    </div>
+    <div class="flex-grow ml-4">
+      {{ $component->message() }}
+    </div>
+  </div>
+  @elseif ($type === 'danger')
+  <div class="flex items-center p-4 text-red-600 bg-red-100 rounded-md">
+    <div class="heroicon-icon">
+      <x-heroicon-o-x-circle class="w-7 h-7" />
+    </div>
+    <div class="flex-grow ml-4">
+      {{ $component->message() }}
+    </div>
+  </div>
+  @else
+  <div class="flex items-center p-4 text-green-600 bg-green-100 rounded-md">
+    <div class="heroicon-icon">
+      <x-heroicon-o-check-circle class="w-7 h-7" />
+    </div>
+    <div class="flex-grow ml-4">
+      {{ $component->message() }}
+    </div>
+  </div>
+  @endif
+</x-alert>
+@endforeach
