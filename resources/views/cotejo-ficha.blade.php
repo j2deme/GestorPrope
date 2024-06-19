9effe7ccf125f3f984d51fc19a06924dade2618f
Jaime Jesús Delgado Meraz <j2deme@gmail.com>
2024-06-18 20:27:44 -0600

    Actualiza cotejo de ficha con datos dinámicos

--- resources/views/cotejo-ficha.blade.php
+++ resources/views/cotejo-ficha.blade.php
@@ -6,49 +6,49 @@
   <x-notification />
 
   <p class="py-2 text-justify">
-    Agradecemos tu preferencia al aplicar para nuevo ingreso al Instituto Tecnológico de Ciudad Valles.
+    A continuación se muestra la información de tu ficha de aspirante, así como el turno seleccionado para el curso.
+    Toma nota de estos datos.
   </p>
+  @if (isset($confirmar) && $confirmar)
   <p class="py-2 text-justify">
-    De acuerdo con los registros, ya seleccionaste previamente un turno para curso propedéutico.
+    Mantente al pendiente del correo electrónico que proporcionaste en tu ficha, ya que por ese medio se te notificará
+    cualquier cambio o información relevante.
   </p>
+  @endif
   <!-- Tabla con datos de la ficha -->
   <div class="overflow-x-auto">
     <table class="min-w-full divide-y divide-gray-200">
       <thead class="bg-gray-50">
         <tr>
-          <th scope="col" class="px-6 py-3 text-xs font-medium tracking-wider text-left text-gray-500 uppercase">
+          <th scope="col" class="px-6 py-3 text-xs font-medium tracking-wider text-center text-gray-500 uppercase">
             Ficha
           </th>
-          <th scope="col" class="px-6 py-3 text-xs font-medium tracking-wider text-left text-gray-500 uppercase">
+          <th scope="col" class="px-6 py-3 text-xs font-medium tracking-wider text-center text-gray-500 uppercase">
             Nombre
           </th>
-          <th scope="col" class="px-6 py-3 text-xs font-medium tracking-wider text-left text-gray-500 uppercase">
-            Carrera
+          <th scope="col" class="px-6 py-3 text-xs font-medium tracking-wider text-center text-gray-500 uppercase">
+            Grupo
           </th>
-          <th scope="col" class="px-6 py-3 text-xs font-medium tracking-wider text-left text-gray-500 uppercase">
-            Turno
-          </th>
-          <th scope="col" class="px-6 py-3 text-xs font-medium tracking-wider text-left text-gray-500 uppercase">
-            Fecha
+          <th scope="col" class="px-6 py-3 text-xs font-medium tracking-wider text-center text-gray-500 uppercase">
+            Horario
           </th>
         </tr>
       </thead>
       <tbody class="bg-white divide-y divide-gray-200">
         <tr>
           <td class="px-6 py-4 whitespace-nowrap">
-            <div class="text-sm text-gray-900">9999</div>
+            <div class="text-sm text-center text-gray-900">{{ $aspirante->ficha }}</div>
+          </td>
+          <td class="px-6 py-4 whitespace-nowrap text-wrap">
+            <div class="text-sm text-center text-gray-900">{{ $aspirante->nombre }}</div>
           </td>
           <td class="px-6 py-4 whitespace-nowrap">
-            <div class="text-sm text-gray-900">Nombre del alumno</div>
+            <div class="text-sm text-center text-gray-900">{{ $grupo->nombre }}</div>
           </td>
           <td class="px-6 py-4 whitespace-nowrap">
-            <div class="text-sm text-gray-900">Ingeniería en Sistemas Computacionales</div>
-          </td>
-          <td class="px-6 py-4 whitespace-nowrap">
-            <div class="text-sm text-gray-900">Matutino</div>
-          </td>
-          <td class="px-6 py-4 whitespace-nowrap">
-            <div class="text-sm text-gray-900">2021-09-01</div>
+            <div class="text-sm text-center text-gray-900">{{ str_pad($grupo->horario->hora_inicio, 2, '0',
+              STR_PAD_LEFT) }} - {{
+              str_pad($grupo->horario->hora_fin, 2, '0', STR_PAD_LEFT) }}</div>
           </td>
         </tr>
       </tbody>
@@ -57,4 +57,9 @@
     <p class="py-2 text-justify">
       Para cualquier aclaración, por favor comunícate al Departamento de Desarrollo Académico.
     </p>
+
+    <a href="{{ route('home') }}"
+      class="inline-flex items-center justify-center w-full px-4 py-2 mb-2 text-base font-medium text-white border border-transparent rounded-md shadow-sm bg-sky-800 hover:bg-sky-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-sky-800 sm:text-sm">
+      Aceptar
+    </a>
 </x-guest-layout>
