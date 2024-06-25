f964d1ad191579754d9279090f5295d43aa9561b
Jaime Jesús Delgado Meraz <j2deme@gmail.com>
2024-06-24 19:23:13 -0600

    Corrección menor

--- resources/views/seleccion-grupo.blade.php
+++ resources/views/seleccion-grupo.blade.php
@@ -22,7 +22,7 @@
         $turno->horario->dias ?? '' }} ({{ str_pad($turno->horario->hora_inicio, 2, '0', STR_PAD_LEFT) }} - {{
         str_pad($turno->horario->hora_fin, 2,
         '0',
-        STR_PAD_LEFT) }}
+        STR_PAD_LEFT) }})
       </a>
     </div>
     @empty
